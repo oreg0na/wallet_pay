73a266792b59b001057e01d59305e24247425e2b
Slava Pogosov <pogoslav1@gmail.com>
2023-11-28 13:06:33 +0300

    fix

---
+++ walletpay/types/base.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+class Response:
+    ...
+
+    @classmethod
+    def from_json(cls, json: dict[str, Any]) -> "Response":
+        raise NotImplementedError
---
+++ walletpay/types/create_response.py
@@ -0,0 +1,3 @@
+from dataclasses import dataclass
+from typing import Any
+from adaptix import Retort, loader, P
