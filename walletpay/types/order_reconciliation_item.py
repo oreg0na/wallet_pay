bfbe6bcbb2a07fae5240950f5609fcf3d3a42f6e
Slava Pogosov <pogoslav1@gmail.com>
2023-11-28 16:44:07 +0300

    working reconciliation_item...

---
+++ walletpay/types/order_reconciliation_item.py
@@ -0,0 +1,7 @@
+from typing import Any
+from dataclasses import dataclass
+from datetime import datetime
+from adaptix import Retort
+from .money_amount import MoneyAmount
+from .payment_option import PaymentOption
+from .enums import OrderStatus
