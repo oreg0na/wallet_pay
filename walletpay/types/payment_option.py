2593b988451dbc77d9c86ca16b396b1da46472df
Slava Pogosov <pogoslav1@gmail.com>
2023-11-28 16:43:21 +0300

    add payment

---
+++ walletpay/types/payment_option.py
@@ -0,0 +1,10 @@
+from decimal import Decimal
+from dataclasses import dataclass
+from .money_amount import MoneyAmount
+
+@dataclass
+class PaymentOption:
+    amount: MoneyAmount
+    amountFee: MoneyAmount
+    amountNet: MoneyAmount
+    exchangeRate: Decimal
