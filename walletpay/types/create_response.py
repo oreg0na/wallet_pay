dc411708aa7fed1fe42996fe6983d27634a7f201
Slava Pogosov <pogoslav1@gmail.com>
2023-11-28 13:20:50 +0300

    work...

--- walletpay/types/create_response.py
+++ walletpay/types/create_response.py
@@ -1,3 +1,5 @@
 from dataclasses import dataclass
 from typing import Any
 from adaptix import Retort, loader, P
+# from .order_preview import OrderPreview
+from .enums import CreateStatus
---
+++ walletpay/types/order_preview.py
@@ -0,0 +1,5 @@
+from typing import Any
+from dataclasses import dataclass
+from adaptix import Retort, loader, P
+from .money_amount import MoneyAmount
+from .enums import OrderStatus
