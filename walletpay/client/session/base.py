c50c8929847d1c8fecd1ff8d2e6c8f9c9f83c777
Slava Pogosov <pogoslav1@gmail.com>
2023-11-30 10:13:07 +0300

    add session base (1.0.6)

--- walletpay/client/session/base.py
+++ walletpay/client/session/base.py
@@ -17,3 +17,73 @@ DEFAULT_HEADERS: Final[dict[str, str]] = {
 }
 
 API_BASE_URL = "https://pay.wallet.tg/wpay/store-api/v1/{}"
+
+class BaseSession(abc.ABC):
+    def __init__(
+        self,
+        json_loads: _JsonLoads = json.loads,
+        timeout: float = DEFAULT_TIMEOUT,
+        headers: dict[str, str] | None = None,
+        api_endpoint: str | None = None,
+    ) -> None:
+        self.json_loads = json_loads
+        self.timeout = timeout
+        self.retort = adaptix.Retort(
+            recipe=[
+                adaptix.name_mapping(skip="__.*"),
+                adaptix.name_mapping(map={"_with": "with"}),
+            ]
+        )
+        self.headers = headers or DEFAULT_HEADERS
+        self.endpoint = api_endpoint or API_BASE_URL
+
+    def check_response(
+        self,
+        method: Method[ResponseType],
+        status_code: int,
+        content: str,
+    ) -> ResponseType:
+        try:
+            json_data = self.json_loads(content)
+        except Exception as e:
+            raise JsonDecodeError(content) from e
+
+        if status_code == 200:
+            try:
+                obj = method.build_response(json_data)
+            except LoadError as e:
+                raise ObjectLoadError(json_data, method.__returning__) from e
+            return obj
+
+        raise ServerError(
+            status_code,
+            content,
+        )
+
+    @abc.abstractmethod
+    async def make_request(
+        self,
+        method: Method[ResponseType],
+    ) -> ResponseType:
+        ...
+
+    @abc.abstractmethod
+    async def close(self) -> None:
+        ...
+
+    async def __aenter__(self) -> BaseSession:
+        return self
+
+    async def __aexit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_value: Optional[BaseException],
+        traceback: Optional[TracebackType],
+    ) -> None:
+        await self.close()
+
+    async def __call__(
+        self,
+        method: Method[ResponseType],
+    ) -> ResponseType:
+        return await self.make_request(method)
