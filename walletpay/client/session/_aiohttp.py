4d28a1d38d2e23c6bd07819db504cf2a19c1028e
Slava Pogosov <pogoslav1@gmail.com>
2023-12-03 23:45:53 +0300

    add 1.0.6

--- walletpay/client/session/_aiohttp.py
+++ walletpay/client/session/_aiohttp.py
@@ -3,3 +3,30 @@ from ...methods.base import ResponseType, Method
 from .base import BaseSession
 
 API_BASE_URL = "https://pay.wallet.tg/wpay/store-api/v1/{}"
+
+class AiohttpSession(BaseSession):
+    def __init__(self):
+        self.session = ClientSession()
+        super().__init__()
+
+    async def make_request(
+        self,
+        method: Method[ResponseType],
+    ) -> ResponseType:
+        url = API_BASE_URL.format(method.__api_method__)
+
+        async with self.session.request(
+            method.__http_method__,
+            url,
+            headers=self.headers,
+            json=method.to_json(),
+            params=method.query_params(),
+        ) as r:
+            return self.check_response(
+                method,
+                status_code=r.status,
+                content=await r.text(),
+            )
+
+    async def close(self) -> None:
+        await self.session.close()
