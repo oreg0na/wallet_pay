3e44a132f0d2c22a718b0821bcb929fc2fc16b21
Slava Pogosov <pogoslav1@gmail.com>
2023-12-03 23:46:33 +0300

    add webhook example

--- examples/webhook.py
+++ examples/webhook.py
@@ -6,3 +6,26 @@ from aiohttp.web_response import Response
 from walletpay.types.update import Update
 
 app = Application()
+
+async def webhook(request: Request) -> Response:
+    headers = request.headers
+
+    text = await request.text()
+
+    sign = Update.compute_signature(
+        environ.get("WP_TEST_KEY"),
+        request.method,
+        request.path,
+        timestamp=headers["WalletPay-Timestamp"],
+        body=text,
+    )
+
+    wp_sign = headers["WalletPay-Signature"]
+
+    print(sign, wp_sign, wp_sign == sign)
+
+    return Response()
+
+app.router.add_post("/", webhook)
+
+run_app(app, host="localhost", port=80)
